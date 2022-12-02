705423f2598b30d28d5778f21befdfc6b61cd9fc
Nathan Kolbas <nathan2000@outlook.com>
2022-12-02 00:34:25 -0600

    SOme style changes

--- lib/pages/toolbox/toolbox.dart
+++ lib/pages/toolbox/toolbox.dart
@@ -10,31 +10,39 @@ class Toolbox extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         const SizedBox(height: 50),
-        const Text("Toolbox",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+        const Text(
+          "Toolbox",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
         const SizedBox(height: 50),
-        ToolboxRow(
-            onTap: () {
-              print("CLI click");
-            },
-            iconData: Icons.terminal,
-            label: "Run Hashcat on CLI"
-        ),
-        const SizedBox(height: 15),
-        ToolboxRow(
-            onTap: () {
-              print("Benchmark click");
-            },
-            iconData: Icons.timer,
-            label: "Run Benchmark"
-        ),
-        const SizedBox(height: 15),
-        ToolboxRow(
-            onTap: () {
-              print("Hashcat Files click");
-            },
-            iconData: Icons.folder,
-            label: "Hashcat Files"
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              ToolboxRow(
+                  onTap: () {
+                    print("CLI click");
+                  },
+                  iconData: Icons.terminal,
+                  label: "Run Hashcat on CLI"
+              ),
+              const SizedBox(height: 15),
+              ToolboxRow(
+                  onTap: () {
+                    print("Benchmark click");
+                  },
+                  iconData: Icons.timer,
+                  label: "Run Benchmark"
+              ),
+              const SizedBox(height: 15),
+              ToolboxRow(
+                  onTap: () {
+                    print("Hashcat Files click");
+                  },
+                  iconData: Icons.folder,
+                  label: "Hashcat Files"
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -56,26 +64,27 @@ class ToolboxRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: themeDark.dividerColor),
-        child: InkWell(
-          onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Icon(iconData),
-                  SizedBox(width: 25),
-                  Text(label,
-                      style:
-                      TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                ]
-            )
-            ,
-          )
-          ,
-        ));
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: themeDark.dividerColor,
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(iconData),
+              const SizedBox(width: 25),
+              Text(
+                label,
+                style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
